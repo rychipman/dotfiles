95911fbf005728f397f4829cea7023b01a1059e9
Ryan Chipman <ryan@ryanchipman.com>
2015-08-15 16:43:34 -0400

    bash: Factor git status logic into function

--- bash/.config/bash/prompt.sh
+++ bash/.config/bash/prompt.sh
@@ -1,6 +1,17 @@
 
 source ~/.config/bash/git-prompt.sh
 
+git_status() {
+    GIT_PS1_SHOWSTASHSTATE=1
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWUNTRACKEDFILES=1
+    GIT_PS1_SHOWCOLORHINTS=true
+    GIT_PS1_DESCRIBE_STYLE="branch"
+    GIT_PS1_SHOWUPSTREAM="git verbose auto"
+    GIT_PS1_HIDE_IF_PWD_IGNORED=1
+    echo "$(__git_ps1 " (%s)")"
+}
+
 set_prompt() {
     lastcmd=$?
     blue="$(tput setaf 33)"
@@ -17,20 +28,13 @@ set_prompt() {
         namecolor="$red"
     fi
 
-    GIT_PS1_SHOWSTASHSTATE=1
-    GIT_PS1_SHOWDIRTYSTATE=1
-    GIT_PS1_SHOWUNTRACKEDFILES=1
-    GIT_PS1_SHOWCOLORHINTS=1
-    GIT_PS1_DESCRIBE_STYLE="branch"
-    GIT_PS1_SHOWUPSTREAM="git verbose auto"
-    GIT_PS1_HIDE_IF_PWD_IGNORED=1
 
     PS1='\n'
     PS1+='\[$namecolor\]\u'
     PS1+='\[$white\]@\[$blue\]\h'
     PS1+=' \[$white\]in \w'
     PS1+='\n'
-    PS1+='\[$gitcolor\]$(__git_ps1 " (%s)")'
+    PS1+='\[$gray\]$(git_status)'
     PS1+='\[$white\]$arrow'
     PS1+='\[$reset\]'
 }
