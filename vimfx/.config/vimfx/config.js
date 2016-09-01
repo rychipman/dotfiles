a3b739b9be100249a7d965bc575cf6e0106cd1a0
Ryan Chipman <ryan@ryanchipman.com>
2016-09-01 10:58:40 -0400

    vimfx: Add fastmail passthroughs, et al.

--- vimfx/.config/vimfx/config.js
+++ vimfx/.config/vimfx/config.js
@@ -1,2 +1,15 @@
 
-console.log("hello, world!");
+let map = (shortcuts, command, custom=false) => {
+  vimfx.set(`${custom ? 'custom.' : ''}mode.normal.${command}`, shortcuts)
+}
+
+//map('e','tab_close');
+
+vimfx.addKeyOverrides(
+    [ location => location.hostname === 'www.fastmail.com',
+        [ 'd', 'm', '.', 'c', 'g', 'j', 'k', '/', 'x', 'r', 'a', 'u', '"', ':', ';', "'", 'n']
+    ]
+);
+
+vimfx.set('mode.normal.stop', '<force><C-c>');
+vimfx.set('mode.normal.enter_mode_ignore', '<force><C-z>');
