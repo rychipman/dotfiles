3531eb8a0e4e61b117affcb73925395500bec192
Ryan Chipman <ryan@ryanchipman.com>
2016-09-01 10:59:23 -0400

    hammerspoon: Add win mgmgt shortcuts

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -2,18 +2,41 @@
 -- show a notification any time this config file is reloaded
 hs.notify.new({title="Hammerspoon", informativeText="Configuration Reloaded"}):send()
 
+--
+local hyper = {"control", "option"}
+
 -- reload config
-hs.hotkey.bind({"control", "option"}, "R", function()
+hs.hotkey.bind(hyper, "R", function()
     hs.reload()
 end)
 
 -- test
-hs.hotkey.bind({"control", "option"}, "W", function()
+hs.hotkey.bind(hyper, "W", function()
     hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
 
-hs.hotkey.bind({"control"}, "Tab", function()
-    hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+-- easy fullscreening
+hs.hotkey.bind(hyper, "F", function()
+    hs.window.focusedWindow():toggleFullScreen()
 end)
 
+-- show Empty desktop
+hs.hotkey.bind(hyper, "H", function()
+    hs.window.desktop():focus()
+end)
+
+-- open iTerm
+hs.hotkey.bind(hyper, "J", function()
+    hs.application.launchOrFocus("iTerm")
+end)
+
+-- open Firefox
+hs.hotkey.bind(hyper, "K", function()
+    hs.application.launchOrFocus("Firefox")
+end)
+
+-- open Slack
+hs.hotkey.bind(hyper, "L", function()
+    hs.application.launchOrFocus("Slack")
+end)
 
