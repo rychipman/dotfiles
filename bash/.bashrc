68298a55f4e07436b9a4fee98bd77a5db834ff37
Ryan Chipman <ryan@ryanchipman.com>
2016-09-19 16:30:53 -0400

    bash: Separate history config into its own file

--- bash/.bashrc
+++ bash/.bashrc
@@ -14,14 +14,11 @@ source ~/.config/bash/functions.sh
 # enables bash completion
 source ~/.config/bash/completion.sh
 
+# source history-related configuration
+source ~/.config/bash/history.sh
+
 # set vim-style keybindings
 set -o vi
-
-# append to history on exit instead of overwriting
-shopt -s histappend
-
-# save multi-line commands as one history entry
-shopt -s cmdhist
 
 # import dircolors
 eval $(dircolors ~/.config/bash/dircolors)
---
+++ bash/.config/bash/history.sh
@@ -0,0 +1,17 @@
+# append to history on exit instead of overwriting
+shopt -s histappend
+
+# save multi-line commands as one history entry
+shopt -s cmdhist
+
+# ignore duplicate commands and commands starting with a space
+export HISTCONTROL=ignorespace:ignoredups
+
+# histfile settings
+export HISTFILE=~/.bash_eternal_history
+export HISTSIZE=
+export HISTFILESIZE=
+export HISTTIMEFORMAT="[%F %T] "
+
+# save every line to history (protects against weird bash exits)
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
