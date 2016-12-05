e0d235e56ac22c1caacf6a8ef65d51c07a6d9f7a
Ryan Chipman <ryan@ryanchipman.com>
2016-12-05 16:11:42 -0500

    mongodb: Add brew openssl paths for scons

--- mongodb/.config/bash/contrib/mongodb.sh
+++ mongodb/.config/bash/contrib/mongodb.sh
@@ -1,4 +1,4 @@
 
 export PATH=$PATH:~/bin/mongodb
 
-export SCONSFLAGS="-j 8 --cache=nolinked --ssl"
+export SCONSFLAGS="-j 8 --cache=nolinked --ssl CPPPATH=/usr/local/Cellar/openssl/1.0.2j/include/ LIBPATH=/usr/local/Cellar/openssl/1.0.2j/lib/"
