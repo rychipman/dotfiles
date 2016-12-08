1b26ec24e6886c6722ccd756e6aaa978d952fe48
Ryan Chipman <ryan@ryanchipman.com>
2016-12-08 15:51:34 -0500

    hammerspoon: Remove unused bindings

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -10,19 +10,9 @@ hs.hotkey.bind(hyper, "R", function()
     hs.reload()
 end)
 
--- test
-hs.hotkey.bind(hyper, "W", function()
-    hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
-
 -- easy fullscreening
 hs.hotkey.bind(hyper, "F", function()
     hs.window.focusedWindow():toggleFullScreen()
-end)
-
--- show Empty desktop
-hs.hotkey.bind(hyper, "H", function()
-    hs.window.desktop():focus()
 end)
 
 -- open iTerm
