34de3c74f2fdfb92dd52f062b9e31fcdb1568f17
Ryan Chipman <ryan@ryanchipman.com>
2018-05-29 18:14:26 -0400

    emacs: Move org-mode config into separate file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -215,40 +215,7 @@
 (use-package org
   :ensure t
   :config
-  (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-  (setq-default org-hide-leading-stars t)
-
-  (setq org-todo-keywords
-        '((sequence "TODO(t)"
-		    "WAITING(w)"
-		    "|"
-		    "DONE(d)"
-		    "CANCELED(c)")))
-
-  (setq org-capture-templates
-        '(("j" "Journal Entry"
-           entry (file+datetree "~/org/journal.org")
-           "* %?")
-	  ("T" "Tickler Entry"
-	   entry (file+headline "~/org/gtd/tickler.org" "Tickler")
-	   "* %i%?\n  %U")
-          ("t" "Task [inbox]"
-           entry (file+headline "~/org/gtd/inbox.org" "Inbox")
-           "* TODO %?"
-           :empty-lines 1
-           :prepend t)))
-
-  (setq org-refile-use-outline-path t)
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-refile-allow-creating-parent-nodes 'confirm)
-  (setq org-refile-targets '((org-agenda-files :maxlevel . 3)))
-
-  (setq org-agenda-files (list "~/org/emacs.org"
-			       "~/org/personal.org"
-			       "~/org/work.org"
-			       "~/org/gtd/inbox.org"
-			       "~/org/gtd/tickler.org"
-			       "~/org/gtd/gtd.org")))
+  (load-file "~/.emacs.d/org.el"))
 
 (use-package eyebrowse
   :ensure t
---
+++ emacs/.emacs.d/org.el
@@ -0,0 +1,31 @@
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(setq-default org-hide-leading-stars t)
+
+(setq org-todo-keywords
+    '((sequence "TODO(t)"
+		"WAITING(w)"
+		"|"
+		"DONE(d)"
+		"CANCELED(c)")))
+
+(setq org-capture-templates
+    '(("j" "Journal Entry"
+	entry (file+datetree "~/org/journal.org")
+	"* %?")
+	("T" "Tickler Entry"
+	entry (file+headline "~/org/tickler.org" "Tickler")
+	"* %i%?\n  %U")
+	("t" "Task [inbox]"
+	entry (file+headline "~/org/inbox.org" "Inbox")
+	"* TODO %?"
+	:empty-lines 1
+	:prepend t)))
+
+(setq org-refile-use-outline-path t)
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+(setq org-refile-targets '((org-agenda-files :maxlevel . 5)))
+
+(setq org-agenda-files (list "~/org/inbox.org"
+			    "~/org/tickler.org"
+			    "~/org/gtd.org"))
