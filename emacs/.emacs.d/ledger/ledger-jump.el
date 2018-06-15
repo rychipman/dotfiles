0311e207e31d5385ae79c134a4537b70bb3283c1
Ryan Chipman <ryan@ryanchipman.com>
2018-06-15 18:01:01 -0400

    emacs: Refactor ledger jumplist to include full xacts

--- emacs/.emacs.d/ledger/ledger-jump.el
+++ emacs/.emacs.d/ledger/ledger-jump.el
@@ -26,11 +26,17 @@
   "Populate 'ledger-xact-jumplist with the elements of 'ledger-xacts that match 'ledger-jumplist-filter."
   (unless ledger-xacts
     (error "No transactions found in ledger-xacts"))
-  (let* ((filtered (seq-filter ledger-jumplist-filter ledger-xacts))
-	 (lines (mapcar 'rpc/ledger-xact-line filtered)))
-    (unless lines
+  (let ((filtered (seq-filter ledger-jumplist-filter ledger-xacts)))
+    (unless filtered
       (warn "no xacts match jumplist filter"))
-    (setq ledger-xact-jumplist lines)))
+    (setq ledger-xact-jumplist filtered)))
+
+(defun rpc/ledger-jump-to-xact (xact)
+  (unless xact
+	(error "Cannot jump to nil xact"))
+  (goto-char (point-min))
+  (forward-line
+   (1- (rpc/ledger-xact-line xact))))
 
 (defun ledger-jump-uncleared ()
   (interactive)
@@ -47,16 +53,14 @@
   (interactive)
   (unless ledger-xact-jumplist
 	(error "No xacts in jumplist"))
-  (goto-char (point-min))
-  (forward-line (1- (car ledger-xact-jumplist))))
+  (rpc/ledger-jump-to-xact (car ledger-xact-jumplist)))
 
 (defun ledger-jumplist-last ()
   "Jump to the last line number in 'ledger-xact-jumplist."
   (interactive)
   (unless ledger-xact-jumplist
 	(error "No xacts in jumplist"))
-  (goto-char (point-min))
-  (forward-line (1- (car (last ledger-xact-jumplist)))))
+  (rpc/ledger-jump-to-xact (car (last ledger-xact-jumplist))))
 
 (defun after-current-line (linum)
   "Return true if LINUM is greater than the current line number."
@@ -67,11 +71,12 @@
   (interactive)
   (unless ledger-xact-jumplist
 	(error "No xacts in jumplist"))
-  (let ((linum (seq-find 'after-current-line ledger-xact-jumplist)))
-	(unless linum
+  (let ((xact (seq-find
+			   '(lambda (xact) (after-current-line (rpc/ledger-xact-line xact)))
+			   ledger-xact-jumplist)))
+	(unless xact
 	  (error "No xacts in jumplist after current line"))
-	(goto-char (point-min))
-	(forward-line (1- linum))))
+	(rpc/ledger-jump-to-xact xact)))
 
 (defun before-current-line (linum)
   "Return true if LINUM is less than the current line number."
@@ -82,8 +87,9 @@
   (interactive)
   (unless ledger-xact-jumplist
 	(error "No xacts in jumplist"))
-  (let ((linum (seq-find 'before-current-line (reverse ledger-xact-jumplist))))
-	(unless linum
+  (let ((xact (seq-find
+			   '(lambda (xact) (before-current-line (rpc/ledger-xact-line xact)))
+			   ledger-xact-jumplist)))
+	(unless xact
 	  (error "No xacts in jumplist before current line"))
-	(goto-char (point-min))
-	(forward-line (1- linum))))
+	(rpc/ledger-jump-to-xact xact)))
