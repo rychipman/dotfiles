d31b20ee21ad72897e427c6e5789a7389c419b4d
Ryan Chipman <ryan@ryanchipman.com>
2018-06-19 10:05:33 -0400

    emacs: Make xact-matching ux more seamless

--- emacs/.emacs.d/ledger/ledger-import.el
+++ emacs/.emacs.d/ledger/ledger-import.el
@@ -8,7 +8,8 @@
 (defun rpc/ledger-match-xact (xact)
   (rpc/ledger-set-jumplist-filter (rpc/ledger-create-amount-matcher xact))
   (ledger-jumplist-first)
-  (hydra-ledger-match/body))
+  (recenter)
+  (hydra-ledger-match-jump/body))
 
 (defun rpc/ledger-find-xact-at-point ()
   (save-excursion
@@ -27,7 +28,7 @@
 	 (t (rpc/ledger-prompt-add-xact xact)))))
 
 (defun rpc/ledger-prompt-add-xact (xact)
-  (when (y-or-n-p "No match found; add xact? ")
+  (if (y-or-n-p "No match found; add xact? ")
 	(let* ((extents (ledger-navigate-find-xact-extents (point)))
 		   (begin (car extents))
 		   (end (cadr extents)))
@@ -35,7 +36,8 @@
 	  (find-file-other-window "~/ledger/test.ledger")
 	  (goto-char (point-max))
 	  (newline)
-	  (yank))))
+	  (yank))
+	(hydra-ledger-match/body)))
 
 ;; TODO: save import and test buffer if not saved
 (defun rpc/ledger-match-imports ()
@@ -45,22 +47,33 @@
   (find-file "~/ledger/import.ledger")
   (hydra-ledger-match/body))
 
-(defhydra hydra-ledger-match ()
+(defhydra hydra-ledger-match
+  (:foreign-keys warn)
   "ledger-match"
   ("j" ledger-navigate-next-xact-or-directive)
   ("k" ledger-navigate-prev-xact-or-directive)
+  ("q" nil :exit t)
   ("RET" rpc/ledger-try-match-xact-at-point :exit t))
 
-(defhydra hydra-ledger-match-jump ()
+(defhydra hydra-ledger-match-jump
+  (:foreign-keys warn)
   "ledger-match-jump"
   ("j" ledger-jumplist-next)
   ("k" ledger-jumplist-prev)
-  ("RET" rpc/ledger-match-choose :exit t))
+  ("q" rpc/ledger-match-jump-quit :exit t)
+  ("RET" rpc/ledger-match-jump-choose :exit t))
 
-(defun rpc/ledger-match-choose ()
+(defun rpc/ledger-match-jump-quit ()
+  (interactive)
+  (select-window (next-window))
+  (hydra-ledger-match/body))
+
+(defun rpc/ledger-match-jump-choose ()
   (interactive)
   (ledger-navigate-beginning-of-xact)
   (end-of-line)
-  (newline-and-indent)
-  (insert ":chosen:")
-  (delete-window))
+  (newline)
+  (insert "    ; :chosen:")
+  (save-buffer)
+  (select-window (next-window))
+  (hydra-ledger-match/body))
