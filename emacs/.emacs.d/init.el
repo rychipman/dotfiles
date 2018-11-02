cd9a05676cfc4c7503ad7714eb3992d65e2fbb88
Ryan Chipman <ryan@ryanchipman.com>
2018-11-02 11:44:56 -0400

    emacs: Remove call to package-initialize

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,7 +22,6 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
