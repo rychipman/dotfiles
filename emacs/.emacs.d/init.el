4365a4f116b2bcf2ab88d5d107208cd7dc43ef38
Ryan Chipman <ryan@ryanchipman.com>
2019-08-01 14:42:09 -0400

    emacs: Fix default font height

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -70,7 +70,7 @@
 (defun zoom-frame-laptop ()
  "Zoom the current frame to an appropriate size for my laptop screen."
  (interactive)
- (set-face-attribute 'default (selected-frame) :height 120))
+ (set-face-attribute 'default (selected-frame) :height 110))
 
 (defun zoom-frame-in ()
  "Zoom in the current frame."
@@ -724,7 +724,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1c1c1c" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Source Code Pro"))))
+ '(default ((t (:inherit nil :stipple nil :background "#1c1c1c" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "nil" :family "Source Code Pro"))))
  '(fringe ((t (:background "#151515"))))
  '(hl-line ((t (:foreground nil :underline nil))))
  '(linum ((t (:inherit (shadow default) :background "#151515"))))
