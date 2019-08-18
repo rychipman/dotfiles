cec1cd4ea1306a56030990066d2a7d0fee823fdf
Ryan Chipman <ryan@ryanchipman.com>
2019-08-18 17:03:04 -0400

    flutter: Create stow config

---
+++ flutter/.config/bash/contrib/flutter.sh
@@ -0,0 +1,6 @@
+
+export PATH="$PATH:~/mobile/flutter/bin"
+export PATH="$PATH:~/mobile/flutter/.pub-cache/bin"
+export PATH="$PATH:~/mobile/android/sdk"
+export PATH="$PATH:~/mobile/android/sdk/platform-tools"
+export ANDROID_HOME='~/mobile/android/sdk/'
