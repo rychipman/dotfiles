9470ae153e796c4aaf4e13611956dcaa8a28da00
Ryan Chipman <ryan@ryanchipman.com>
2019-09-04 14:59:08 -0400

    emacs: Install yasnippet

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -318,6 +318,12 @@
 (use-package graphviz-dot-mode
   :ensure t)
 
+(use-package yasnippet
+  :ensure t)
+
+(use-package yasnippet-snippets
+  :ensure t)
+
 (use-package lsp-mode
   :ensure t
   :commands lsp
@@ -709,7 +715,7 @@
  '(lsp-ui-peek-enable nil t)
  '(package-selected-packages
    (quote
-	(graphviz-dot-mode dart-mode use-package-ensure-system-package forge key-chord crux ryo-modal perspective company-restclient restclient yaml-mode git-timemachine dumb-jump smart-jump toml-mode cargo cargo-mode persp-mode tablist elfeed mu4e-alert rust-mode gotest worf ledger-mode smartparens git-gutter-fringe hydra go-eldoc company epresent evil-magit diff-hl badger-theme counsel-projectile projectile cider clojure-mode syndicate evil-surround go-mode eyebrowse magit which-key general use-package)))
+	(yasnippet-snippets graphviz-dot-mode dart-mode use-package-ensure-system-package forge key-chord crux ryo-modal perspective company-restclient restclient yaml-mode git-timemachine dumb-jump smart-jump toml-mode cargo cargo-mode persp-mode tablist elfeed mu4e-alert rust-mode gotest worf ledger-mode smartparens git-gutter-fringe hydra go-eldoc company epresent evil-magit diff-hl badger-theme counsel-projectile projectile cider clojure-mode syndicate evil-surround go-mode eyebrowse magit which-key general use-package)))
  '(safe-local-variable-values
    (quote
 	((rpc/compile/build-command . "cd $(git rev-parse --show-toplevel) && go install cmd/mongosqld/mongosqld.go")
