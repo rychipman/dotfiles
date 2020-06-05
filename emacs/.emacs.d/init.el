122297def44cf0582b28a09e1d37b0d58ce9c6d2
Ryan Chipman <ryan@ryanchipman.com>
2020-06-05 13:31:43 -0400

    emacs: Put elpa dir into .local

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -25,6 +25,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(setq package-user-dir "~/.local/share/emacs/elpa")
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
