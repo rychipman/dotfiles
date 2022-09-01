beb346405eb4b681297169bbb1ad5c2809bf678c
Ryan Chipman <ryan@ryanchipman.com>
2022-09-01 15:22:00 -0400

    remove stows for a bunch of software I no longer use

--- flutter/.config/bash/contrib/flutter.sh
+++
@@ -1,6 +0,0 @@
-
-export PATH="$PATH:~/mobile/flutter/bin"
-export PATH="$PATH:~/mobile/flutter/.pub-cache/bin"
-export PATH="$PATH:~/mobile/android/sdk"
-export PATH="$PATH:~/mobile/android/sdk/platform-tools"
-export ANDROID_HOME='~/mobile/android/sdk/'
