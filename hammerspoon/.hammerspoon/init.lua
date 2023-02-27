dc385be622f10bec5047a00d0b5972e19a8ccb69
Ryan Chipman <ryan@ryanchipman.com>
2023-02-27 13:25:27 -0500

    hammerspoon: Remove unused binds

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -9,16 +9,6 @@ hs.hotkey.bind(hyper, "R", function()
     hs.reload()
 end)
 
--- sleep displays
-hs.hotkey.bind(hyper, "delete", function()
-    os.execute("pmset displaysleepnow")
-end)
-
--- easy fullscreening
-hs.hotkey.bind(hyper, "F", function()
-    hs.window.focusedWindow():toggleFullScreen()
-end)
-
 apps = {
   {
     name = "Firefox",
